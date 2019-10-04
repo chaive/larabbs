1e7938b00d2f4fcd7b04969bb69c072703f03d15
chaive <chaive@gmail.com>
2019-10-04 21:20:04 +0800

    基础页面结构

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+  <div class="container">
+{{--    Branding Image--}}
+    <a href="{{ url('/') }}" class="navbar-brand">
+      LaraBBS
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Tollge navigation">
+      <span class="navbar-toggle-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="nabarSupportedContent">
+{{--        left side of navbar--}}
+      <ul class="navbar-nav mr-auto">
+
+      </ul>
+{{--      right side of navbar--}}
+      <ul class="navbar-nav navbar-right">
+        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">注册</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
