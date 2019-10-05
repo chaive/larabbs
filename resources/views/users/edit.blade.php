4ca3b63594159083afdadb45d1d363bd9cbbeba5
chaive <chaive@gmail.com>
2019-10-05 15:09:41 +0800

    上传头像

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,7 +8,7 @@
          <h4><i class="glyphicon glyphicon-edit"></i>编辑个人资料</h4>
        </div>
        <div class="card-body">
-         <form action="{{ route('users.update',$user->id) }}" method="POST" accept-charset="UTF-8">
+         <form action="{{ route('users.update',$user->id) }}" method="POST" enctype="multipart/form-data" accept-charset="UTF-8">
            @csrf
            @method('PUT')
            @include('shared._error')
@@ -23,6 +23,16 @@
              <div class="form-group">
                <label for="introduction-field">个人简介</label>
                <textarea type="text" name="introduction" id="introduction-field" value="{{ old('introduction' ,$user->introduction) }}" class="form-control"></textarea>
+
+               <div class="form-group mb-4">
+                 <label for="" class="avatar-label">用户头像</label>
+                 <input type="file" name="avatar" class="form-control-file">
+
+                 @if($user->avatar)
+                   <br>
+                   <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="thumbnail img-responsive" width="200">
+                   @endif
+               </div>
              </div>
            <div class="well well-sm">
              <button class="btn btn-primary">保存</button>
