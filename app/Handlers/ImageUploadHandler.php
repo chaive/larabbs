92105fc9e3f099f2911f17c1378c6f8e6e97684d
chaive <chaive@gmail.com>
2019-10-06 19:43:23 +0800

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,19 +1,20 @@
 <?php
 namespace App\Handlers;
 
-use Illuminate\Support\Str;
+use Image;
+use Str;
 
 class ImageUploadHandler
 {
     protected $allowed_ext = ["png", "jpg", "gif", "jpeg"];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
 
         $upload_path = public_path() . '/' . $folder_name;
 
-        $extension = strtolower($file->getClientOriginalExtension()) ? : 'png';
+        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
 
         $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
 
@@ -23,8 +24,27 @@ class ImageUploadHandler
 
         $file->move($upload_path, $filename);
 
+        if ($max_width && $extension != 'gif') {
+
+            // 此类中封装的函数，用于裁剪图片
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return[
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+       $image = Image::make($file_path);
+
+       $image->resize($max_width, null, function ($constraint) {
+
+           $constraint->aspectRatio();
+
+           $constraint->upsize();
+       });
+       $image->save();
     }
 }
