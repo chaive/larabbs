9f405b41db33edf48723d30837b4ad3a7e27550d
chaive <chaive@gmail.com>
2019-10-07 18:51:21 +0800

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
 <!--  style -->
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  @yield('styles')
 </head>
 <body>
 <div id="app" class="{{ route_class() }}-page">
@@ -26,5 +27,6 @@
 </div>
 <!-- script -->
 <script src="{{ mix('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
