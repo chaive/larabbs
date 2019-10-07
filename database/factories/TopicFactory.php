247eb7ac8a3fbd81a87f5d490fc0c2c260f83655
chaive <chaive@gmail.com>
2019-10-07 09:12:06 +0800

    生成话题骨架

---
+++ database/factories/TopicFactory.php
@@ -0,0 +1,10 @@
+<?php
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+$factory->define(App\Models\Topic::class, function (Faker $faker) {
+    return [
+        // 'name' => $faker->name,
+    ];
+});
